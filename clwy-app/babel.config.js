d64f65a91895e4691831ec36d326a3940eef8ead
赵鸿飞 <zhf5256@qq.com>
2025-11-02 15:42:58 +0800

    feat: 配置别名

---
+++ clwy-app/babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    Plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+          },
+        },
+      ],
+    ],
+  }
+}
